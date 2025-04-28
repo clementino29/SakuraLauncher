87bf91b57355f360cba2c1953a1c3272da88f3e6
clementino29 <clement.unsc29@gmail.com>
2025-04-28 14:23:26 +0200

    Update package + background image

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-exports.REMOTE_DISTRO_URL = 'https://drive.google.com/uc?export=download&id=1GcXwI4CYAoAdP0rni2q98QiRQNpZoLNg'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/clementino29/skra-distribution/refs/heads/main/distribution.json'
 //exports.REMOTE_DISTRO_URL = 'https://drive.google.com/uc?export=download&id=1bjSUzsHqZLgv9Ozheg4C0w6cSzELJNVj'
 
 const api = new DistributionAPI(
